aa4b052d7872a8b145b87200916b8f6c7bebea29
Mauricio <mauriciotostes@yahoo.com.br>
2026-03-30 21:23:35 -0300

    Implementa botões CRUD e adiciona barra de títulos com geração de relatório em PDF

--- sistema_hotel.py
+++ sistema_hotel.py
@@ -2,8 +2,78 @@ from tkinter import *
 from tkinter import ttk
 import sqlite3
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Image
+import webbrowser
+
+
 root = Tk()
 root.iconbitmap("icone.ico")
+
+class Relatorios():
+    def printCliente(self):
+        webbrowser.open("cliente.pdf")
+    def geraRelatCliente(self):
+        self.c = canvas.Canvas("cliente.pdf")
+
+        self.codigoRel = self.codigo_entry.get()
+        self.CPFRel = self.CPF_entry.get()
+        self.nomeRel = self.nome_entry.get()
+        self.data_nascimentoRel = self.data_nascimento_entry.get()
+        self.sexoRel = self.sexo_entry.get()
+        self.telefoneRel = self.telefone_entry.get()
+        self.cepRel = self.cep_entry.get()
+        self.ruaRel = self.rua_entry.get()
+        self.numeroRel = self.numero_entry.get()
+        self.complementoRel = self.complemento_entry.get()
+        self.bairroRel = self.bairro_entry.get()
+        self.cidadeRel = self.cidade_entry.get()
+        self.estadoRel = self.estado_entry.get()
+        self.observacoesRel = self.observacoes_entry.get()
+
+        self.c.setFont("Courier-Bold", 20)
+        self.c.drawString(200, 800, 'Ficha do Cliente')
+
+        self.c.setFont("Courier-Bold", 14)
+        self.c.drawString(50, 760, 'Código............:')
+        self.c.drawString(50, 740, 'CPF...............:')
+        self.c.drawString(50, 720, 'Nome..............:')
+        self.c.drawString(50, 700, 'Data de Nascimento:')
+        self.c.drawString(50, 680, 'Sexo..............:')
+        self.c.drawString(50, 660, 'Telefone..........:')
+        self.c.drawString(50, 640, 'CEP...............:')
+        self.c.drawString(50, 620, 'Rua...............:')
+        self.c.drawString(50, 600, 'Número............:')
+        self.c.drawString(50, 580, 'Complemento.......:')
+        self.c.drawString(50, 560, 'Bairro............:')
+        self.c.drawString(50, 540, 'Cidade............:')
+        self.c.drawString(50, 520, 'Estado............:')
+        self.c.drawString(50, 500, 'Observações.......:')
+
+        self.c.setFont("Courier", 14)
+        self.c.drawString(220, 760, self.codigoRel)
+        self.c.drawString(220, 740, self.CPFRel)
+        self.c.drawString(220, 720, self.nomeRel)
+        self.c.drawString(220, 700, self.data_nascimentoRel)
+        self.c.drawString(220, 680, self.sexoRel)
+        self.c.drawString(220, 660, self.telefoneRel)
+        self.c.drawString(220, 640, self.cepRel)
+        self.c.drawString(220, 620, self.ruaRel)
+        self.c.drawString(220, 600, self.numeroRel)
+        self.c.drawString(220, 580, self.complementoRel)
+        self.c.drawString(220, 560, self.bairroRel)
+        self.c.drawString(220, 540, self.cidadeRel)
+        self.c.drawString(220, 520, self.estadoRel)
+        self.c.drawString(220, 500, self.observacoesRel)
+
+       # self.c.rect(20, 480, 550, 4, fill=True, stroke=False) -> linha ou borda
+
+        self.c.showPage()
+        self.c.save()
+        self.printCliente()
 
 class Funcs():
     def limpa_cliente(self):
@@ -109,8 +179,31 @@ class Funcs():
         self.desconecta_bd()
         self.limpa_cliente()
         self.select_lista()
+    def altera_cliente(self):
+        self.variaveis()
+        self.conecta_bd()
+        self.cursor.execute(""" UPDATE clientes SET CPF = ?, nome = ?, data_nascimento = ?, sexo = ?, telefone = ?, cep = ?, rua = ?, numero = ?, complemento = ?, bairro = ?, cidade = ?, estado = ?, observacoes = ? WHERE cod = ? """, (self.CPF, self.nome, self.data_nascimento, self.sexo, self.telefone, self.cep, self.rua, self.numero, self.complemento, self.bairro, self.cidade, self.estado, self.observacoes, self.codigo))
+        self.conn.commit()
 
-class Application(Funcs):
+        self.desconecta_bd()
+        self.select_lista()
+        self.limpa_cliente()
+    def busca_cliente(self):
+        self.conecta_bd()
+        self.listaCli.delete(*self.listaCli.get_children())
+
+        self.nome_entry.insert(END, '%')
+        nome = self.nome_entry.get()
+        self.cursor.execute(
+            """SELECT cod, CPF, nome, data_nascimento, sexo, telefone, cep, rua, numero, complemento, bairro, cidade, estado, observacoes FROM clientes WHERE nome LIKE '%s' ORDER BY nome ASC""" % nome)
+        busca_nomeCli = self.cursor.fetchall()
+        for i in busca_nomeCli:
+            self.listaCli.insert("", END, values=i)
+        self.limpa_cliente()
+        self.desconecta_bd()
+
+
+class Application(Funcs, Relatorios):
     def __init__(self):
         self.root = root
         self.tela()
@@ -119,6 +212,7 @@ class Application(Funcs):
         self.lista_frame2()
         self.montaTabelas()
         self.select_lista()
+        self.Menus()
         root.mainloop()
     def tela(self):
         self.root.title("Cadastro de Clientes")
@@ -140,14 +234,14 @@ class Application(Funcs):
         self.bt_limpar.place(relx=0.05, rely=0.05, relwidth=0.1, relheight=0.08)
 
         # Botão Buscar logo ao lado
-        self.bt_buscar = Button(self.frame_1, text="Buscar", font=("DejaVu Sans Mono", 14, "bold"), bd=3)
+        self.bt_buscar = Button(self.frame_1, text="Buscar", font=("DejaVu Sans Mono", 14, "bold"), bd=3, command=self.busca_cliente)
         self.bt_buscar.place(relx=0.17, rely=0.05, relwidth=0.1, relheight=0.08)
 
         # Os demais podem continuar mais à direita
         self.bt_novo = Button(self.frame_1, text="Novo", font=("DejaVu Sans Mono", 14, "bold"), bd=3, command=self.add_cliente)
         self.bt_novo.place(relx=0.6, rely=0.05, relwidth=0.1, relheight=0.08)
 
-        self.bt_alterar = Button(self.frame_1, text="Alterar", font=("DejaVu Sans Mono", 14, "bold"), bd=3)
+        self.bt_alterar = Button(self.frame_1, text="Alterar", font=("DejaVu Sans Mono", 14, "bold"), bd=3, command= self.altera_cliente)
         self.bt_alterar.place(relx=0.72, rely=0.05, relwidth=0.1, relheight=0.08)
 
         self.bt_apagar = Button(self.frame_1, text="Apagar", font=("DejaVu Sans Mono", 14, "bold"), bd=3, command=self.deleta_cliente)
@@ -265,6 +359,24 @@ class Application(Funcs):
         self.listaCli.configure(yscroll=self.scrollLista.set)
         self.scrollLista.place(relx=0.96, rely=0.1, relwidth=0.02, relheight=0.85)
         self.listaCli.bind("<Double-1>", self.OnDoubleClick)       
+    def Menus(self):
+        menubar = Menu(self.root)
+        self.root.config(menu=menubar)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu2 = Menu(menubar, tearoff=0)
+
+        def Quit(): self.root.destroy()
+
+        menubar.add_cascade(label= "Opções", menu= filemenu)
+        menubar.add_cascade(label= "Relatórios", menu= filemenu2)
+
+        filemenu.add_command(label= "Sair", command= Quit)
+        filemenu.add_command(label= "Limpa Cliente", command= self.limpa_cliente)
+
+        filemenu2.add_command(label= "Ficha do Cliente", command= self.geraRelatCliente)
+
+
+        
 
 
 
